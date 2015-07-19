f81fa89a0704ff2d43d8a25e68b5e4f3d376241d
JannaShaftan <shaftan.j@husky.neu.edu>
2015-07-19 01:04:41 -0700

    Angular SPA

---
+++ jannaAngular/app.js
@@ -0,0 +1,38 @@
+var myApp = angular.module('AngelHack', ['ui.router']);
+myApp.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/index');
+  $stateProvider
+
+  .state('one', {
+    url: '/one',
+    templateUrl: 'one.html'
+  })
+  .state('two', {
+    url: '/two',
+    templateUrl: 'two.html'
+  })
+  .state('three', {
+    url: '/three',
+    templateUrl: 'three.html'
+  })
+  .state('four', {
+    url: '/four',
+    templateUrl: 'four.html'
+  })
+  .state('five', {
+    url: '/five',
+    templateUrl: 'five.html'
+  })
+
+});
+
+myApp.controller('MainCtrlr',
+  ['$scope', function($scope) {
+  $scope.greeting = 'Hola!';
+}]);
+
+myApp.controller('navCtrlr',
+  ['$scope','$location', function($scope, $location) {
+    $scope.$location = $location;
+}]);
+
